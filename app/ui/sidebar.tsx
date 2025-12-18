3bbb9b32f3989e78388069507c7db1ba8d5fabcf
Le Dai Thanh <thanh261220@gmail.com>
2025-12-18 12:34:24 +0700

    fix 1 ti

--- app/ui/sidebar.tsx
+++ app/ui/sidebar.tsx
@@ -117,6 +117,7 @@ export default function Sidebar({
                                 video: g.video,
                                 slug: g.slug,
                             }))
+                            .slice(0, 12);
 
                         console.log("Processed recent games list:", games);
                         setRecentGames(games)
